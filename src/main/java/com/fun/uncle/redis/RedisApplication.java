32b955a52c8d608a2acf4530dba9fc0e605e6d07
ruanjiayu <1005572910@qq.com>
2020-07-20 01:25:25 +0800

    初始化项目

---
+++ src/main/java/com/fun/uncle/redis/RedisApplication.java
@@ -0,0 +1,20 @@
+package com.fun.uncle.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*
+* @Description 对于redis相关学习
+* @Author: fan
+* @DateTime: 2020/7/20 1:20 上午
+* @Version: 0.0.1-SNAPSHOT
+*/
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fun/uncle/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.uncle.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
